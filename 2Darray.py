02f36ce43494c2196bcf5adda45fc14c807ecdcb
ShyrusThapa <shyrusthapa160@gmail.com>
2026-04-06 09:49:21 +0545

    Basics of 2D

---
+++ 2Darray.py
@@ -0,0 +1,6 @@
+import numpy as anything
+l1=[1,2,3]
+l2=[4,5,6]
+arr_two_d= anything.array([l1,l2])
+print(arr_two_d) #printing_2D_Arrays
+print("This is",arr_two_d.ndim,"dimensional array") #printing the dimension
